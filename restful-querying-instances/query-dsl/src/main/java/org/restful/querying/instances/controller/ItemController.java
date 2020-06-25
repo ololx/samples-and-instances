52437ed64d4f58ea7fa085a576f71309b247c1bf
Alexander A. Kropotin <ololx@icloud.com>
2020-06-25 09:57:22 +0500

    chore(restful-querying-instances) actualize files history

--- restful-querying-instances/query-dsl/src/main/java/org/restful/querying/instances/controller/ItemController.java
+++ restful-querying-instances/query-dsl/src/main/java/org/restful/querying/instances/controller/ItemController.java
@@ -24,6 +24,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.restful.querying.instances.model.entity.Item;
 import org.restful.querying.instances.model.entity.ItemDetail;
 import org.restful.querying.instances.repository.ItemRepository;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.querydsl.binding.QuerydslPredicate;
 import org.springframework.data.web.PagedResourcesAssembler;

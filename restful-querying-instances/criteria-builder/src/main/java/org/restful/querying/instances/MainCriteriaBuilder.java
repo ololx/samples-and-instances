fda31137728d9547322427f1db424734fc287fc0
Alexander A. Kropotin <ololx@icloud.com>
2020-06-25 10:35:23 +0500

    chore(restful-querying-instances) renaim main conf class for each modelule

--- restful-querying-instances/criteria-builder/src/main/java/org/restful/querying/instances/MainCriteriaBuilder.java
+++ restful-querying-instances/criteria-builder/src/main/java/org/restful/querying/instances/MainCriteriaBuilder.java
@@ -4,18 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
-@EnableSpringDataWebSupport
 @SpringBootApplication
-public class MainConfiguration extends SpringBootServletInitializer {
+public class MainCriteriaBuilder extends SpringBootServletInitializer {
 
     public static void main(String[] args) throws Exception {
-        SpringApplication.run(MainConfiguration.class, args);
+        SpringApplication.run(MainCriteriaBuilder.class, args);
     }
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(MainConfiguration.class);
+        return application.sources(MainCriteriaBuilder.class);
     }
 }

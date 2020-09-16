7742fe02ae97155de8b45fe6ef1db496071b3026
Alexander A. Kropotin <ololx@icloud.com>
2020-09-16 09:51:45 +0500

    feature(OrganizationControllerUnitTest.java) add find method test

--- restful-test-instances/controller-test/src/test/java/org/restful/test/instances/controller/test/controller/OrganizationControllerUnitTest.java
+++ restful-test-instances/controller-test/src/test/java/org/restful/test/instances/controller/test/controller/OrganizationControllerUnitTest.java
@@ -1,5 +1,6 @@
 package org.restful.test.instances.controller.test.controller;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
@@ -18,11 +19,16 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.util.LinkedMultiValueMap;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Optional;
 
 import static org.hibernate.validator.internal.util.Contracts.assertNotNull;
 import static org.hibernate.validator.internal.util.Contracts.assertTrue;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
@@ -78,11 +84,11 @@ public class OrganizationControllerUnitTest {
     @Test
     public void create_positive_whenRequestIsValid_thenSuccessfulCreated() throws Exception {
         OrganizationDetail expectedOrganizationRequest = OrganizationDetail.builder()
-                .name(java.util.Optional.of("WCorp"))
+                .name(Optional.of("WCorp"))
                 .build();
         OrganizationDetail expectedOrganizationResponse = OrganizationDetail.builder()
-                .uid(java.util.Optional.of(1L))
-                .name(java.util.Optional.of("WCorp"))
+                .uid(Optional.of(1L))
+                .name(Optional.of("WCorp"))
                 .build();
         when(organizationService.create(expectedOrganizationRequest)).thenReturn(expectedOrganizationResponse);
 
@@ -116,12 +122,12 @@ public class OrganizationControllerUnitTest {
     public void update_positive_whenRequestIsValid_thenSuccessfulUpdated() throws Exception {
         Long expectedOrganizationUidRequest = 1L;
         OrganizationDetail expectedOrganizationRequest = OrganizationDetail.builder()
-                .uid(java.util.Optional.of(expectedOrganizationUidRequest))
-                .name(java.util.Optional.of("WCorp"))
+                .uid(Optional.of(expectedOrganizationUidRequest))
+                .name(Optional.of("WCorp"))
                 .build();
         OrganizationDetail expectedOrganizationResponse = OrganizationDetail.builder()
-                .uid(java.util.Optional.of(1L))
-                .name(java.util.Optional.of("WCorp"))
+                .uid(Optional.of(1L))
+                .name(Optional.of("WCorp"))
                 .build();
         when(organizationService.update(expectedOrganizationUidRequest, expectedOrganizationRequest))
                 .thenReturn(expectedOrganizationResponse);
@@ -174,6 +180,60 @@ public class OrganizationControllerUnitTest {
 
         verify(organizationService).delete(anyLong());
         verify(organizationService).delete(expectedOrganizationUidRequest);
+        assertNotNull(actualOrganizationResponse, "Что-то пошло не так");
+        assertTrue(
+                actualOrganizationResponse.equals(expectedOrganizationResponse),
+                "Ожидаемый и фактический результаты отличаются - что-то пошло не так!"
+        );
+    }
+
+    /**
+     * Find positive when request is valid then successful found.
+     *
+     * @throws Exception the exception
+     */
+    @Test
+    public void find_positive_whenRequestIsValid_thenSuccessfulFound() throws Exception {
+        List<Long> expectedUidRequest = Collections.singletonList(1L);
+        List<String> expectedNameRequest = Collections.singletonList("WCorp");
+        List<String> expectedInnRequest = Collections.singletonList("01");
+        List<String> expectedKppRequest = Collections.singletonList("01");
+        List<String> expectedAddressRequest = Collections.singletonList(".ell road");
+        List<OrganizationDetail> expectedOrganizationResponse = Collections.singletonList(
+                OrganizationDetail.builder()
+                        .uid(Optional.ofNullable(expectedUidRequest.get(0)))
+                        .name(Optional.ofNullable(expectedNameRequest.get(0)))
+                        .inn(Optional.ofNullable(expectedInnRequest.get(0)))
+                        .kpp(Optional.ofNullable(expectedKppRequest.get(0)))
+                        .address(Optional.ofNullable(expectedAddressRequest.get(0)))
+                        .build()
+        );
+        when(organizationService.find(anyList(), anyList(), anyList(), anyList(), anyList()))
+                .thenReturn(expectedOrganizationResponse);
+
+        MvcResult mvcResult = mvc.perform(
+                get("/organizations")
+                .param("uid", expectedUidRequest.stream().map(p -> String.valueOf(p)).toArray(String[]::new))
+                .param("name", expectedNameRequest.stream().toArray(String[]::new))
+                .param("inn", expectedInnRequest.stream().toArray(String[]::new))
+                .param("kpp", expectedKppRequest.stream().toArray(String[]::new))
+                .param("address", expectedAddressRequest.stream().toArray(String[]::new))
+        )
+                .andExpect(status().is2xxSuccessful())
+                .andReturn();
+        List<OrganizationDetail> actualOrganizationResponse = this.objectMapper.readValue(
+                mvcResult.getResponse().getContentAsString(),
+                new TypeReference<List<OrganizationDetail>>(){}
+        );
+
+        verify(organizationService).find(anyList(), anyList(), anyList(), anyList(), anyList());
+        verify(organizationService).find(
+                expectedUidRequest,
+                expectedNameRequest,
+                expectedInnRequest,
+                expectedKppRequest,
+                expectedAddressRequest
+        );
         assertNotNull(actualOrganizationResponse, "Что-то пошло не так");
         assertTrue(
                 actualOrganizationResponse.equals(expectedOrganizationResponse),

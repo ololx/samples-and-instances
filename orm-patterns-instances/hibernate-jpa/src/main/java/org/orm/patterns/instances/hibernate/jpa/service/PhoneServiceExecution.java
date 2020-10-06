7e89a88ed77c80a61b3ada348ea85360897e2344
Alexander A. Kropotin <ololx@icloud.com>
2020-10-06 09:39:54 +0500

    feature(PhoneSrviceExecution.java) add phone service execution

---
+++ orm-patterns-instances/hibernate-jpa/src/main/java/org/orm/patterns/instances/hibernate/jpa/service/PhoneServiceExecution.java
@@ -0,0 +1,77 @@
+package org.orm.patterns.instances.hibernate.jpa.service;
+
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
+import org.orm.patterns.instances.commons.mapping.CustomModelMapper;
+import org.orm.patterns.instances.commons.model.detail.PersonDetail;
+import org.orm.patterns.instances.commons.model.detail.PhoneDetail;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Optional;
+
+/**
+ * The type Person service execution.
+ */
+@Slf4j
+@RequiredArgsConstructor
+@FieldDefaults(
+        level = AccessLevel.PRIVATE,
+        makeFinal = true
+)
+@Component
+public class PhoneServiceExecution implements ApplicationListener<ApplicationReadyEvent> {
+
+    /**
+     * The Person service.
+     */
+    PersonService personService;
+
+    /**
+     * The Phone service.
+     */
+    PhoneService phoneService;
+
+    /**
+     * On application event.
+     *
+     * @param event the event
+     */
+    @Override
+    public void onApplicationEvent(final ApplicationReadyEvent event) {
+
+        //create new
+        this.createExecution();
+
+        return;
+    }
+
+    /**
+     * Create execution.
+     */
+    private void createExecution() {
+        try {
+            PersonDetail createPersonRequest = PersonDetail.builder()
+                    .firstName(Optional.ofNullable("Person"))
+                    .lastName(Optional.ofNullable("Personson"))
+                    .age(Optional.ofNullable(12))
+                    .build();
+            PersonDetail createPersonResponse = this.personService.create(createPersonRequest);
+            log.info("\u001B[35m" + "Receive the created Person data - {}", createPersonResponse);
+
+            PhoneDetail createPhoneRequest = PhoneDetail.builder()
+                    .personId(Optional.ofNullable(createPersonResponse.getId().orElse(1L)))
+                    .number(Optional.ofNullable("88000088080"))
+                    .build();
+            PhoneDetail createPhoneResponse = this.phoneService.create(createPhoneRequest);
+            log.info("\u001B[35m" + "Receive the created Phone data - {}", createPhoneResponse);
+        } catch (CustomModelMapper.MappingException e) {
+            log.debug("Couldn't create the new Person, because - {}", e.getMessage());
+        }
+    }
+}

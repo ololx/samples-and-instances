6938c97696ddc28b9b9b64acd87b7f6647ced6e0
Alexander A. Kropotin <ololx@icloud.com>
2020-10-19 18:54:15 +0500

    feature(validation) add custom phone validation example

---
+++ orm-patterns-instances/active-jdbc/src/main/java/org/orm/patterns/instances/active/jdbc/service/validation/PhoneValidator.java
@@ -0,0 +1,25 @@
+package org.orm.patterns.instances.active.jdbc.service.validation;
+
+import org.javalite.activejdbc.Model;
+import org.javalite.activejdbc.validation.ValidatorAdapter;
+import org.orm.patterns.instances.active.jdbc.model.entity.Person;
+
+public class PhoneValidator extends ValidatorAdapter {
+
+    @Override
+    public void validate(Model model) {
+        boolean valid = true;
+        valid = validatePersonAge(model);
+
+        if(!valid) model.addValidator(this, "phone_owner_validation");
+
+        return;
+    }
+
+    private boolean validatePersonAge(Model model) {
+        Person person = null;
+        if ((person = model.parent(Person.class)) != null && person.getInteger("age") < 14) return false;
+
+        return true;
+    }
+}

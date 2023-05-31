e41b11949c4758b9586eb63470b4e10b3956c4c4
a-kropotin <a.kropotin@pixonic.com>
2023-05-31 21:43:02 +0300

    chore(*) implement gage factory

--- prometheus-client-metrix-examples/src/main/java/io/github/ololx/samples/PrometheusExampleApp.java
+++ prometheus-client-metrix-examples/src/main/java/io/github/ololx/samples/PrometheusExampleApp.java
@@ -1,10 +1,13 @@
 package io.github.ololx.samples;
 
 import io.github.ololx.samples.metrics.CounterFactory;
+import io.github.ololx.samples.metrics.GaugeFactory;
 import io.prometheus.client.Counter;
 import io.prometheus.client.exporter.HTTPServer;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 import java.util.Map;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
@@ -22,27 +25,59 @@ public class PrometheusExampleApp implements AutoCloseable {
 
     static final CounterFactory counterFactory = new CounterFactory();
 
-    static HTTPServer server;
+    static final GaugeFactory gaugeFactory = new GaugeFactory();
+
+    static final HTTPServer server;
+
+    static {
+        try {
+            server = new HTTPServer.Builder()
+                    .withPort(8080)
+                    .build();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     public static void main(String[] args) throws IOException {
         PrometheusExampleApp app = new PrometheusExampleApp();
-        server = new HTTPServer.Builder()
-                .withPort(8080)
-                .build();
         app.start();
     }
 
     private void start() {
-        executorService.scheduleAtFixedRate(this::countExecutions, 0, 1, TimeUnit.SECONDS);
+        executorService.scheduleAtFixedRate(() -> {
+            this.countExecutions();
+            this.gaugeExecutionsDate();
+        }, 0, 5, TimeUnit.SECONDS
+        );
     }
 
     private void countExecutions() {
+        var now = LocalDateTime.now();
         var executions = counterFactory.getOrCreate(
                 "execution_count",
-                Map.of("author", "guide-man", "application", this.getClass().getName())
+                Map.of(
+                        "author", "guide-man",
+                        "application", this.getClass().getName(),
+                        "now", now.truncatedTo(ChronoUnit.MINUTES).toString()
+                )
         );
         executions.inc();
         System.out.println("Count: " + executions.get());
+    }
+
+    private void gaugeExecutionsDate() {
+        var now = LocalDateTime.now();
+        var executions = gaugeFactory.getOrCreate(
+                "execution_gauge",
+                Map.of(
+                        "author", "guide-man",
+                        "application", this.getClass().getName(),
+                        "now", now.truncatedTo(ChronoUnit.MINUTES).toString()
+                )
+        );
+        executions.set(now.getSecond());
+        System.out.println("Gauge: " + executions.get());
     }
 
     @Override

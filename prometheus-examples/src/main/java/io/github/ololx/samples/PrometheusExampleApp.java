a94edb30f6193a90a508c040236524a04298a686
a-kropotin <a.kropotin@pixonic.com>
2023-05-31 16:09:33 +0300

    chore(*) first initial commit

---
+++ prometheus-examples/src/main/java/io/github/ololx/samples/PrometheusExampleApp.java
@@ -0,0 +1,14 @@
+package io.github.ololx.samples;
+
+/**
+ * project deduplication-service-example
+ * created 27.05.2023 12:48
+ *
+ * @author Alexander A. Kropotin
+ */
+public class PrometheusExampleApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
